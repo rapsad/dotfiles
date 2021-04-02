5be9146ec777783da044af6728cdcaa4dcd72301
Dawid Półchłopek <devilzaba@gmail.com>
2021-04-02 21:46:04 +0200

    Fixup for pre-install.sh script

--- pre-install.sh
+++ pre-install.sh
@@ -6,6 +6,6 @@ if [ ! -d "$OMZDIR" ]; then
   echo 'Installing oh-my-zsh'
   /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
-  echo 'Updating oh-my-zsh'
-  upgrade_oh_my_zsh
+  echo '=== update Oh-My-ZSH ==='
+  env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
 fi
