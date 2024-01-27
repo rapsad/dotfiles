cd6802e41c6126319956cbaeb8c271d163f95efa
Dawid Półchłopek <devilzaba@gmail.com>
2024-01-27 23:07:35 +0100

    Add gvm installation

--- pre-install.sh
+++ pre-install.sh
@@ -3,4 +3,7 @@
 # install rust with cargo
 curl https://sh.rustup.rs -sSf | sh
 
+# install gvm
+curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | sh
+
 brew bundle --file=./Brewfile
